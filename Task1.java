da7e886c9780e117c6f0ce1b52e4c01fdeda0099
KirR <kirillabduldjalilov@gmail.com>
2024-08-03 16:47:31 +0300

    Task 1

---
+++ Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static void PrintThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
