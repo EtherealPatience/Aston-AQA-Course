12b108dddaa8e7222b8de6a66d72ed176ccefbd0
KirR <kirillabduldjalilov@gmail.com>
2024-08-04 19:28:02 +0300

    Code refactoring

--- Task8.java
+++ Task8.java
@@ -1,6 +1,6 @@
 public class Task8 {
-    public static void printStringCyclically(String s, int a){
-        for(int i = 0; i < a; i++)
+    public static void printStringCyclically(String s, int count){
+        for(int i = 0; i < count; i++)
             System.out.println(s);
     }
 }
