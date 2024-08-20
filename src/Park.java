ffdaf02eea5f7fb0fb661ad7e8c52f66638f510b
KirR <kirillabduldjalilov@gmail.com>
2024-08-20 15:56:53 +0300

    Create class Park

--- src/Park.java
+++ src/Park.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+
 public class Park {
+    public ArrayList<Attraction> attractionList = new ArrayList<Attraction>();
+
+    public void addAttraction(Attraction attraction){
+        attractionList.add(attraction);
+    }
+
+    public void deleteAttraction(Attraction attraction){
+        attractionList.remove(attraction);
+    }
+
+    public void showAttractionsInfo(){
+        for(Attraction attraction : attractionList){
+            attraction.showInfo();
+            System.out.println();
+        }
+    }
 
 }
