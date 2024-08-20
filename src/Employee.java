332f4439aab6ba490cfbab81ff04bb9ce70a4ef1
KirR <kirillabduldjalilov@gmail.com>
2024-08-20 15:39:17 +0300

    Code refactoring

--- src/Employee.java
+++ src/Employee.java
@@ -1,21 +1,24 @@
 public class Employee {
     String fullName;
     String position;
+    String email;
     String phone;
     int salary;
     int age;
 
-    public Employee(String _fullName, String _position, String _phone, int _salary, int _age){
+    public Employee(String _fullName, String _position, String _email, String _phone, int _salary, int _age){
         fullName = _fullName;
         position = _position;
+        email = _email;
         phone = _phone;
         salary = _salary;
         age = _age;
     }
 
     public void showInfo(){
-        System.out.println("Сотрудник " + fullName + "\n" +
+        System.out.println("Сотрудник " + fullName + ":\n" +
                 "занимаемая должность: " + position + "\n" +
+                "email: " + email + "\n" +
                 "возраст: " + age + "\n" +
                 "мобильный телефон: " + phone + "\n" +
                 "зарплата: " + salary);
--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,27 @@ public class Main {
     }
 
     public static Employee[] fillEmployeesArray(){
+        //Done in order to avoid fixed values in the program
         int length = 5;
+        int n = 0;
         Employee[] employeeArray = new Employee[length];
-        employeeArray[0] = new Employee("Abduldjalilov Kirill Junior","AQA Java engineer","+3751234567",4000,22);
-        employeeArray[1] = new Employee("Vatko Sergey Ibragimovich","Java engineer","+3752234523",3500,25);
-        employeeArray[2] = new Employee("Soldatenko Dmitrii Vyacheslavovich","Team lead","+3751234567",6000,29);
-        employeeArray[3] = new Employee("Akter Elizabeth Sergeevna","HR","+3755423124",3500,22);
-        employeeArray[4] = new Employee("Chikoi Adam Pavlovich","AQA Java engineer","+3757654321",4000,22);
+        employeeArray[n++] = new Employee("Abduldjalilov Kirill Junior","AQA Java engineer",
+                "kirillabduldjalilov@gmail.com","+3751234567",4000,22);
+        employeeArray[n++] = new Employee("Vatko Sergey Ibragimovich","Java engineer",
+                "vatkosergey@gmail.com","+3752234523",3500,25);
+        employeeArray[n++] = new Employee("Soldatenko Dmitrii Vyacheslavovich","Team lead",
+                "soldatenkodima@gmail.com","+3751234567",6000,29);
+        employeeArray[n++] = new Employee("Akter Elizabeth Sergeevna","HR",
+                "akterliza@gmail.com","+3755423124",3500,22);
+        employeeArray[n++] = new Employee("Chikoi Adam Pavlovich","AQA Java engineer",
+                "chikoiadam@gmail.com","+3757654321",4000,22);
         return employeeArray;
     }
 
     public static void displayEmployeesArrayInfo(Employee[] employeeArray){
         for(Employee emp : employeeArray){
             emp.showInfo();
-            System.out.println("\n");
+            System.out.println();
         }
     }
 }
