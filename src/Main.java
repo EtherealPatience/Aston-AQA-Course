af0097854d8b13c000a4cd7fea93f3ba357bf65a
KirR <kirillabduldjalilov@gmail.com>
2024-08-26 15:55:29 +0300

    [Task 1] Create MyArraySizeException

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        printMatrix(new String[][] {{"1", "2", "3", "10"}, {"4", "5", "6", "10"}, {"7", "8", "9", "10"}, {"10", "11", "12", "10"}});
+
+    }
+
+    public static void printMatrix(String[][] matrix){
+        try{
+            //Matrix check
+            for(int i = 0; i < matrix.length; i++)
+                for (int j = 0; j < matrix[i].length; j++)
+                    if(matrix.length != 4 || matrix[i].length != 4)
+                        throw new MyArraySizeException();
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        catch (MyArraySizeException e){
+            System.out.printf("Исключение: размерность матрицы не 4x4");
+        }
     }
 }
