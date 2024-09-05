0e0e9a01603a44ca3c101902a187857a362b648e
KirR <kirillabduldjalilov@gmail.com>
2024-09-05 15:26:41 +0300

    Create lesson11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        String[] arrayNames = new String[]{
+                "Kirill", "Vitaly","Pavel", "Kirill",
+                "Lelouch", "Griffith", "Kirill", "Vitaly",
+                "Osaka", "Pavel", "Vitaly", "Kirill",
+                "Griffith", "Mikasa", "Frieren", "Lelouch"
+        };
+
+
     }
 }
