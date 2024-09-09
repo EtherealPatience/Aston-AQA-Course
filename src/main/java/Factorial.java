448f728a3aca1d862732214f64d4a1b7ac8e3a7c
KirR <kirillabduldjalilov@gmail.com>
2024-09-09 17:00:07 +0300

    create Factorial class

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,3 +1,12 @@
 public class Factorial {
-
+    public static int getFactorial(int num){
+        if(num < 0){
+            throw new IllegalArgumentException("У отрицательных чисел нет факториала");
+        }
+        int factorial = 1;
+        for(int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        try{
+            System.out.println(Factorial.getFactorial(-10));
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
