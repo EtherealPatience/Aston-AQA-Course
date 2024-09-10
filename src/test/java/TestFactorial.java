05a52fceddac4783a2770d56bdb81ed1c5251cfa
KirR <kirillabduldjalilov@gmail.com>
2024-09-10 11:20:18 +0300

    Create positive number testing

--- src/test/java/TestFactorial.java
+++ src/test/java/TestFactorial.java
@@ -1,3 +1,27 @@
-public class TestFactorial {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.testng.Assert;
 
+public class TestFactorial {
+    @ParameterizedTest(name = "Факториал {0} равен {1}")
+    @DisplayName("Тест положительных чисел для факториала")
+    @CsvSource(value = {
+            "0, 1",
+            "1, 1",
+            "2, 2",
+            "3, 6",
+            "4, 24",
+            "5, 120",
+            "6, 720",
+            "7, 5040",
+            "8, 40320",
+            "9, 362880",
+            "10, 3_628_800",
+            "11, 39_916_800",
+            "12, 479_001_600"
+    })
+    public void testFactorialPositiveNumbers(int input, int expected){
+        Assert.assertEquals(Factorial.getFactorial(input), expected);
+    }
 }
