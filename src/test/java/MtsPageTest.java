616e17ea07eca0b14f07291917bcf3a53b3419e1
KirR <kirillabduldjalilov@gmail.com>
2024-10-01 16:42:41 +0300

    Update pom.xml + add some allure annotations

--- src/test/java/MtsPageTest.java
+++ src/test/java/MtsPageTest.java
@@ -1,12 +1,17 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.junit5.AllureJunit5AnnotationProcessor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebElement;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
+@ExtendWith(AllureJunit5AnnotationProcessor.class)
 public class MtsPageTest extends SeleniumInitializer{
 
     String phone = "297777777";
@@ -15,12 +20,16 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка названия блока")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется актуальное название указанного блока с фактическим")
     public void checkNameBlock(){
         Assertions.assertEquals("Онлайн пополнение\nбез комиссии", mtsPage.getBlockName());
     }
 
     @Test
     @DisplayName("Проверка наличия логотипов платежных систем")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется отображение логотипов платежных систем. В случае, если логотип не отображается - выводится содержимое атрибута alt")
     public void checkLogoDisplay(){
         List<WebElement> logoList = mtsPage.getLogoList();
         for(WebElement logo : logoList){
@@ -30,6 +39,8 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка работы ссылки")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется переходит ли пользователь по клику ссылки на страницу с актуальной информацией")
     public void checkLinkWork(){
         String expectedTitle = "Порядок оплаты и безопасность интернет платежей";
         Assertions.assertNotNull(mtsPage.getServiceLink());
@@ -40,6 +51,8 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка работоспособности кнопки")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется переходит ли пользователь на новую страницу при вводе валидных данных в поля")
     public void checkButtonWork(){
         String expectedText = "или используйте карту";
 
@@ -49,6 +62,8 @@ public class MtsPageTest extends SeleniumInitializer{
     }
 
     @ParameterizedTest(name = "У элемента списка {0} присутствуют плейсхолдеры {1}, {2}, {3}")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется отображение плейсхолдеров")
     @DisplayName("Проверка плейсхолдеров")
     @CsvSource({
             "Услуги связи, Номер телефона, Сумма, E-mail для отправки чека",
@@ -70,6 +85,8 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка корректности отображения суммы")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется правильно ли переносится сумма, указанная в поле при онлайн пополнении, на странице")
     public void checkCorrectDisplaySum(){
         String expectedText = sum + ".00 BYN";
 
@@ -80,6 +97,8 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка корректности отображения суммы на кнопке")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется правильно ли переносится сумма, указанная в поле при онлайн пополнении, на кнопке")
     public void checkCorrectDisplaySumButton(){
         String expectedText = "Оплатить " + sum + ".00 BYN";
 
@@ -90,6 +109,8 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка корректности отображения номера телефона")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется правильно ли переносится номер телефона")
     public void checkCorrectDisplayPhone(){
         String expectedText = "Оплата: Услуги связи Номер:375" + phone;
 
@@ -99,6 +120,8 @@ public class MtsPageTest extends SeleniumInitializer{
     }
 
     @ParameterizedTest(name = "Надпись в незаполненном поле {0} корректно отображается")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется правильно ли отображается надпись в незаполненных полях")
     @DisplayName("Проверка корректности надписей в незаполненных полях в iframe")
     @CsvSource({
             "Номер карты", "Срок действия", "CVC", "Имя держателя (как на карте)"
@@ -112,6 +135,8 @@ public class MtsPageTest extends SeleniumInitializer{
 
     @Test
     @DisplayName("Проверка наличия иконок платежных систем")
+    @Owner("Kirill Abduldjalilov")
+    @Description("Проверяется отображение иконок платежных систем")
     public void checkIconsPaymentDisplay(){
         mtsPage.setValues(phone, sum, email);
 
